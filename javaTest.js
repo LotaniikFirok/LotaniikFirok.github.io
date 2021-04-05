8386b8b1de57970d003f655392eed9f5a4f4a3ba
LotaniikFirok <72287003+LotaniikFirok@users.noreply.github.com>
2021-04-05 00:12:35 -0400

    12 to 24

--- javaTest.js
+++ javaTest.js
@@ -136,7 +136,7 @@
             //Adjusts Hz of the output oscillator
                 //Declare min/max in hertz
                 hertzTest = ((24)*(setVal/setValMax))+28;
-                hertz = (2**((hertzTest.toFixed(0)-49)/12))*440
+                hertz = (2**((hertzTest.toFixed(0)-49)/24))*440
                 hertz = hertz.toFixed(2);
             for (i = 3; i < edit.length; i++){
                 if (edit[i] == true)
